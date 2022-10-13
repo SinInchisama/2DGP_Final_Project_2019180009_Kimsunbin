f3e01b0029eedd3251d274ce3d97a274287a9f03
SinInchisama <sun98390123@gmail.com>
2022-10-13 22:36:35 +0900

    자전거 타는 모션 완성

--- play_state.py
+++ play_state.py
@@ -4,26 +4,27 @@ from Npc import Maping
 from Map import init_map
 import game_framework
 
-direct,round,mode,running,Map_change =0,0,0,None,None
+direct,round,mode,running,Map_change,speed =0,0,0,None,None,None
 hero = None
 
 def enter():
-    global direct,round,mode,running,Map_change,hero
+    global direct,round,mode,running,Map_change,hero,speed
     direct = -1  # 방향
-    round = 4  # 맵 변경에 사용하는 변수
+    round = 5  # 맵 변경에 사용하는 변수
     mode = 0  # 각종 모드에 사용하는 변수
     running = True
     Map_change = False  # 맵변경시 깜빡이는 효과를 내기 위한 변수
+    speed = 0.07
     init_map()
-    #hero = Character.Hero(18,3350,32,32,304,304,304,304,0,0)
-    hero = Character.Hero(18,3350,32,32,624, 368, 1248, 352,0,0)
+    hero = Character.Hero(18,3350,32,32,304,304,304,304,0,0)
+    #hero = Character.Hero(18,3350,32,32,624, 368, 1248, 352,0,0)
     hero.character_image = pico2d.load_image('./resource/image/Character.png')
 
 def handle_events():
     global direct
     global hero
     global mode
-    global round
+    global round,speed
     events = get_events()
     for event in events:
 
@@ -44,6 +45,14 @@ def handle_events():
                 direct = 0
                 hero.movey -= 1
                 hero.movex = 0
+
+            elif event.key == SDLK_x:  # 윗 버튼 눌리면
+                if speed == 0.07:
+                    hero.pngy = 3315
+                    speed = 0.04
+                else:
+                    hero.pngy = 3350
+                    speed = 0.07
 
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_LEFT:  # 왼쪽 버튼 눌리면
@@ -75,7 +84,7 @@ def Hero_working(direct):
             elif(hero.movey !=0 ):
                 hero.chy += hero.movey * 8
             draw()
-            delay(0.05)
+            delay(speed)
         # print(hero.mapx, hero.mapy, hero.chx, hero.chy,Maping[round].Nowx, Maping[round].Nowy,hero.movex, hero.movey,)
 
 
