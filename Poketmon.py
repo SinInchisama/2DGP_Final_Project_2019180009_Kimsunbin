229b5cc7daad1b0a1cbf49c7d086a192a62e0261
SinInchisama <sun98390123@gmail.com>
2022-10-15 20:06:34 +0900

    Poketmon 기본 구현

---
+++ Poketmon.py
@@ -0,0 +1,11 @@
+class Poketmon:
+    def __init__(self, Pngx, Pngy,MaxHp,NowHp,MaxPattack,MaxPdefense,MaxSattack,MaxSdefense,Speed):
+        self.Pngx = Pngx
+        self.Pngy = Pngy
+        self.MaxHp = MaxHp
+        self.NowHp = NowHp
+        self.MaxPattack = MaxPattack
+        self.MaxPdefense = MaxPdefense
+        self.MaxSattack = MaxSattack
+        self.MaxSdefense = MaxSdefense
+        self.Speed = Speed
