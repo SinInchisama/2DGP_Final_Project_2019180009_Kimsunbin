f9397a0f233705a48b9d4253fcd85eb863458c45
SinInchisama <sun98390123@gmail.com>
2022-10-18 14:25:00 +0900

    Skill_Class 기본구성

--- Skill_Data.py
+++ Skill_Data.py
@@ -1,24 +1,25 @@
 Skill_Data = None
 
 class Dbuf:
-    def __init__(self,PPowerbuf,SPowerbuf,PDefensebuf,SDefensebuf,Speedbuf,Poison,Paralysis,Sleep,Burn):
+    def __init__(self):         # buf가 양수면 그만큼 능력치가 상승함, 상태이상은 TRUE,FALSE로 판단
         self.PPowerbuf = 0
         self.SPowerbuf = 0
         self.PDefensebuf = 0
         self.SDefensebuf = 0
         self.Speedbuf = 0
-        self.Poison = 0
-        self.Paralysis = 0
-        self.Sleep = 0
-        self.Burn = 0
+        self.Poison = False
+        self.Paralysis = False
+        self.Sleep = False
+        self.Burn = False
 
 class Skill:
-    def __init__(self,Damage,Hill,Daccur,Bufaccur,PPowerbuf,SPowerbuf,PDefensebuf,SDefensebuf,Speedbuf,Poison,Paralysis,Sleep,Burn):
-        Dbuf.__init__(self,PPowerbuf,SPowerbuf,PDefensebuf,SDefensebuf,Speedbuf,Poison,Paralysis,Sleep,Burn)
-        self.Damage = Damage
-        self.Hill = Hill
-        self.Daccur =Daccur
-        self.Bufaccur = 10
+    def __init__(self,Damage,Hill,Daccur):
+        Dbuf.__init__(self)
+        self.Damage = Damage        # 말그대로 데미지
+        self.Hill = Hill            # 힐하는 스킬에 적용
+        self.Daccur =Daccur         # 기술마다 적중률
+        self.Bufaccur = 10          # 특정하지 않는 경우 DBuf적중률은 10%
+        self.Me = False             # 스킬이 나한테 써지는지 판단하는 변수 보통은 false
 
 def init_skill():
     global Skill_Data
