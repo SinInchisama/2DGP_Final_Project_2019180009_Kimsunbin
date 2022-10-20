0342e84bd717846ec2824bbe789015605a0e6f2a
SinInchisama <sun98390123@gmail.com>
2022-10-21 00:01:56 +0900

    맵 출력 코드변경
    맵 배열 수정중 주말내로 끝내는게 목표
    포켓몬 기술은 4주차로 밀릴듯

---
+++ Test.py
@@ -0,0 +1,13 @@
+import Map
+from pico2d import *
+
+open_canvas(640,576)
+
+Map.init_map()
+
+while True:
+    clear_canvas()
+    Map.Draw_Map()
+    update_canvas()
+    delay(0.01)
+    get_events()
