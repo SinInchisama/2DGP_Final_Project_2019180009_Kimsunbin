b9b7c91aa16b3136f823d466cdd4f4f1bad101ca
SinInchisama <sun98390123@gmail.com>
2022-11-03 13:40:26 +0900

    순서가 잘못되있어서 변경

--- Battle.py
+++ Battle.py
@@ -1,12 +1,11 @@
 from pico2d import *
 import game_framework
-
 import Poketmon
 import Skill_Data
 from random import randint
 
 def Speed_check(My,Enermy):
-    if(My.Speed * (1 + 1 / 4 * Enermy.ChangeSp) > My.Speed * (1 + 1 / 4 * Enermy.ChangeSp)):
-        return [1,0]
+    if((My.Speed * (1 + 1 / 4 * My.ChangeSp)) > (Enermy.Speed * (1 + 1 / 4 * Enermy.ChangeSp))):
+        return [0,1,None]
     else:
-        return [0,1]
+        return [1,0,None]
