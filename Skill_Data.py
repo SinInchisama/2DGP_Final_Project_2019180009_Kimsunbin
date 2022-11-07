b3b866a87b6bf200791b3eade68f3f512d586d01
SinInchisama <sun98390123@gmail.com>
2022-11-07 23:48:18 +0900

    스킬 이름 추가

--- Skill_Data.py
+++ Skill_Data.py
@@ -35,8 +35,10 @@ class S_Skill:
 class body_blow(P_Skill):                 # 몸통박치기
     def __init__(self):
         P_Skill.__init__(self,40,100,35,'Normal')
+        self.name = 'body blow'
 
     def Draw(self,check,My):
+        print('1111')
         for i in range(2,0,-1):
             clear_canvas()
 
@@ -52,9 +54,10 @@ class body_blow(P_Skill):                 # 몸통박치기
 class crying_sound:             # 울음소리
     def __init__(self):
         self.PPowerbuf = -1
-        self.Maxpp = 35,
+        self.Maxpp = 35
         self.Daccur = 100
         self.type = 'Normal'
+        self.name = 'crying sound'
 
     def Use(self, My, Enermy):
         if(My.ChangePa >-3):
@@ -88,6 +91,7 @@ class Razor_Leaf(S_Skill):               # 잎날가르기
 
     def __init__(self):
         S_Skill.__init__(self, 55, 95, 25, 'Grass')
+        self.name = 'Razor Leaf'
     def Draw(self, check,My):
         for a in range(0,300,2):
             clear_canvas()
@@ -116,6 +120,7 @@ class Poison_Powder:            # 독가루
         self.Daccur = 75
         self.Maxpp = 25
         self.type = 'Grass'
+        self.name = 'Poison Powder'
 
     def Use(self, My, Enermy):
         if(My.ailment == None):
@@ -140,6 +145,7 @@ class Synthesis:                # 광합성
         self.Hill = 100
         self.Maxpp = 5
         self.type = 'Grass'
+        self.name = 'Synthesis'
 
     def Use(self):
         pass
@@ -147,6 +153,7 @@ class Synthesis:                # 광합성
 class Body_Slam(P_Skill):            # 누르기
     def __init__(self):
         P_Skill.__init__(self, 85, 100, 15, 'Normal')
+        self.name = 'Body Slam'
 
     def Draw(self,check,My):
         for i in range(2,0,-1):
@@ -164,6 +171,7 @@ class Body_Slam(P_Skill):            # 누르기
 class Ember(S_Skill):                # 불꽃세례
     def __init__(self):
         S_Skill.__init__(self, 40, 100, 25, 'Fire')
+        self.name = 'Ember'
 
     def Use(self, My, Enermy):
         My.Hp = My.Hp - int((self.Damage * Enermy.Sattack * (1 + 1 / 4 * Enermy.ChangeSa) * (Enermy.level * 2 / 5 + 2) / My.Sdefense * (1 + 1 / 4 * My.ChangeSd) / 50 + 2) * My.Type_check(self.type))
@@ -186,6 +194,7 @@ class Ember(S_Skill):                # 불꽃세례
 class Quick_Attack(P_Skill):         # 전광석화
     def __init__(self):
         P_Skill.__init__(self, 40, 100, 30, 'Normal')
+        self.name = 'Quick Attack'
 
     def Draw(self,check,My):
         for i in range(2,0,-1):
@@ -203,6 +212,7 @@ class Quick_Attack(P_Skill):         # 전광석화
 class Flame_Wheel(P_Skill):          # 화염자동차
     def __init__(self):
         P_Skill.__init__(self, 60, 100, 25, 'Fire')
+        self.name = 'Flame Wheel'
     def Use(self,My,Enermy):
         My.Hp = My.Hp - int((self.Damage * Enermy.Pattack * (1 + 1 / 4 * Enermy.ChangePa) * (Enermy.level * 2 / 5 + 2) / My.Pdefense * (1 + 1 / 4 * My.ChangePd) / 50 + 2) * My.Type_check(self.type))
         if(random.randint(0,100)<10 and My.ailment == None):
@@ -230,6 +240,7 @@ class Leer:                 # 째려보기
         self.Maxpp = 35
         self.type = 'Normal'
         self.Daccur = 100
+        self.name = 'Leer'
 
     def Use(self, My, Enermy):
         if (My.ChangePd > -3):
@@ -260,6 +271,7 @@ class Leer:                 # 째려보기
 class Scratch(P_Skill):              # 할퀴기
     def __init__(self):
         P_Skill.__init__(self, 40, 100, 35, 'Normal')
+        self.name = 'Scratch'
 
     def Draw(self,check,My):
         for i in range(2,0,-1):
@@ -276,6 +288,7 @@ class Scratch(P_Skill):              # 할퀴기
 class Rage(P_Skill):                 # 분노
     def __init__(self):
         P_Skill.__init__(self, 20, 100, 20, 'Normal')
+        self.name = 'Rage'
 
     def Draw(self,check,My):
         for i in range(2,0,-1):
@@ -293,6 +306,7 @@ class Rage(P_Skill):                 # 분노
 class Water_Gun(S_Skill):            # 물대포
     def __init__(self):
         S_Skill.__init__(self, 40, 100, 25, 'Water')
+        self.name = 'Water Gun'
 
     def Draw(self, check, My):
         for a in range(0, 200, 2):
@@ -316,6 +330,7 @@ class Water_Gun(S_Skill):            # 물대포
 class Bite(P_Skill):                 # 물기
     def __init__(self):
         P_Skill.__init__(self, 60, 100, 25, 'Dark')
+        self.name = 'Bite'
 
     def Draw(self,check,My):
         for i in range(2,0,-1):
@@ -335,6 +350,7 @@ class Scary_Face:           # 겁나는 얼굴
         self.Maxpp = 10
         self.Daccur = 90
         self.type = 'Normal'
+        self.name = 'Scary Face'
 
     def Use(self, My, Enermy):
         if (My.ChangeSp > -2):
@@ -367,6 +383,7 @@ class Scary_Face:           # 겁나는 얼굴
 class Slash(P_Skill):                # 베어가르기
     def __init__(self):
         P_Skill.__init__(self, 70, 100, 20, 'Normal')
+        self.name = 'Slash'
 
     def Draw(self,check,My):
         for i in range(2,0,-1):
@@ -386,6 +403,7 @@ class Screech:              # 싫은소리
         self.Maxpp = 40
         self.Daccur = 85
         self.type = 'Normal'
+        self.name = 'Screech'
 
     def Use(self, My, Enermy):
 
@@ -419,6 +437,7 @@ class Screech:              # 싫은소리
 class Hydro_Pump(S_Skill):           # 하이드로펌프
     def __init__(self):
         S_Skill.__init__(self, 120, 80, 5, 'Water')
+        self.name = 'Hydro Pump'
 
     def Draw(self, check,My):
 
@@ -443,6 +462,7 @@ class Hydro_Pump(S_Skill):           # 하이드로펌프
 class Slam(P_Skill):                 # 힘껏치기
     def __init__(self):
         P_Skill.__init__(self, 80, 75, 20, 'Normal')
+        self.name = 'Slam'
 
     def Draw(self,check,My):
         for i in range(2,0,-1):
@@ -463,6 +483,7 @@ class Amnesia:              # 망각술
         self.Maxpp = 20
         self.Daccur = 100
         self.type = 'Esper'
+        self.name = 'Amnesia'
 
     def Use(self, My, Enermy):
         if (Enermy.ChangeSd < 2):
@@ -496,6 +517,7 @@ class String_Shot:          # 실뿜기
         self.Maxpp = 20
         self.type = 'Bug'
         self.Daccur = 100
+        self.name = 'String_Shot'
 
     def Use(self, My, Enermy):
         if (My.ChangeSp > -2):
@@ -516,6 +538,7 @@ class Harden:               # 단단해지기
         self.Maxpp = 40
         self.type = 'Normal'
         self.Daccur = 100
+        self.name = 'Harden'
 
     def Use(self, My, Enermy):
         if (Enermy.ChangePd < 3):
@@ -546,6 +569,7 @@ class Harden:               # 단단해지기
 class Confusion(S_Skill):            # 염동력
     def __init__(self):
         S_Skill.__init__(self, 50, 100, 25, 'Esper')
+        self.name = 'Confusion'
 
     def Draw(self, check, My):
         for a in range(0, 200, 2):
@@ -565,6 +589,7 @@ class Stun_Spore:               # 저리가루
         self.Maxpp = 15
         self.Daccur = 75
         self.type = 'Grass'
+        self.name = 'Stun Spore'
 
     def Use(self, My, Enermy):
         if (random.randint(0, 75) <= self.Daccur):
@@ -591,6 +616,7 @@ class Sleep_Powder:  # 수면가루
         self.Maxpp = 15
         self.Daccur = 75
         self.type = 'Grass'
+        self.name = 'Sleep Powder'
 
     def Use(self, My, Enermy):
         if (random.randint(0, 75) <= self.Daccur):
@@ -614,6 +640,7 @@ class Sleep_Powder:  # 수면가루
 class Gust(S_Skill):     # 바람일으키기
     def __init__(self):
         S_Skill.__init__(self, 40, 100, 35, 'Flight')
+        self.name = 'Gust'
 
     def Draw(self,check,My):
 
@@ -631,6 +658,7 @@ class Gust(S_Skill):     # 바람일으키기
 class Psybeam(S_Skill):  # 환상빔
     def __init__(self):
         S_Skill.__init__(self, 65, 100, 20, 'Esper')
+        self.name = 'Psybeam'
 
     def Draw(self, check, My):
         for a in range(0, 200, 2):
@@ -648,6 +676,7 @@ class Psybeam(S_Skill):  # 환상빔
 class Poison_Sting(S_Skill):    # 독침
     def __init__(self):
         S_Skill.__init__(self, 15, 100, 35, 'Poison')
+        self.name = 'Poison Sting'
 
     def Use(self, My, Enermy):
         if (random.randint(0, 100) <= self.Daccur):
@@ -673,6 +702,7 @@ class Agility:        # 고속이동
         self.Maxpp = 30
         self.type = 'Esper'
         self.Daccur = 100
+        self.name = 'Agility'
 
     def Use(self, My, Enermy):
         if (Enermy.ChangePd < 2):
@@ -703,6 +733,7 @@ class Agility:        # 고속이동
 class Pursuit(P_Skill):        # 따라가때리기
     def __init__(self):
         P_Skill.__init__(self, 40, 100, 20, 'Dark')
+        self.name = 'Pursuit'
 
     def Draw(self,check,My):
         for i in range(2,0,-1):
@@ -720,6 +751,7 @@ class Pursuit(P_Skill):        # 따라가때리기
 class Pin_Missile(P_Skill):      # 바늘 미사일
     def __init__(self):
         P_Skill.__init__(self, 25, 95, 100, 'Bug')
+        self.name = 'Pin Missile'
     def Use(self,My,Enermy):
         i = random.randint(2,4)
         count  = 0
@@ -743,6 +775,7 @@ class Pin_Missile(P_Skill):      # 바늘 미사일
 class Wing_Attack(P_Skill):     # 날개치기
     def __init__(self):
         P_Skill.__init__(self, 60, 100, 35, 'Bug')
+        self.name = 'Wing Attack'
 
     def Draw(self,check,My):
         for i in range(2,0,-1):
@@ -760,6 +793,7 @@ class Wing_Attack(P_Skill):     # 날개치기
 class Thundershock:    # 전기쇼크
     def __init__(self):
         S_Skill.__init__(self, 40, 100, 30, 'Electric')
+        self.name = 'Thundershock'
 
     def Use(self, My, Enermy):
         My.Hp = My.Hp - int((self.Damage * Enermy.Sattack * (1 + 1 / 4 * Enermy.ChangeSa) * (Enermy.level * 2 / 5 + 2) / My.Sdefense * (1 + 1 / 4 * My.ChangeSd) / 50 + 2) * My.Type_check(self.type))
@@ -772,6 +806,7 @@ class Tail_Whip:      # 꼬리 흔들기
         self.Maxpp = 30
         self.type = 'Normal'
         self.Daccur = 100
+        self.name = 'Tail Whip'
 
     def Use(self, My, Enermy):
         if (Enermy.ChangePd > -2):
@@ -804,6 +839,7 @@ class Thunder_Wave:     # 전기 충격파
         self.Daccur = 90
         self.Maxpp = 20
         self.type = 'Electric'
+        self.name = 'Thunder Wave'
 
     def Use(self, My, Enermy):
         if (My.ailment == None):
@@ -812,15 +848,20 @@ class Thunder_Wave:     # 전기 충격파
 class Thunderbolt(S_Skill):      # 10만볼트
     def __init__(self):
         S_Skill.__init__(self, 90, 100, 15, 'Electric')
+        self.name = 'Thunderbolt'
 
     def Use(self, My, Enermy):
         My.Hp = My.Hp - int((self.Damage * Enermy.Sattack * (1 + 1 / 4 * Enermy.ChangeSa) * (Enermy.level * 2 / 5 + 2) / My.Sdefense * (1 + 1 / 4 * My.ChangeSd) / 50 + 2) * My.Type_check(self.type))
         if (random.randint(0, 100) < 10):
             My.ailment = 'Paralysis'
 
+    def Draw(self, check, My):
+        pass
+
 class Thunder(S_Skill):          # 번개
     def __init__(self):
         S_Skill.__init__(self, 110, 70, 10, 'Electric')
+        self.name = 'Thunder'
 
     def Use(self, My, Enermy):
         My.Hp = My.Hp - int((self.Damage * Enermy.Sattack * (1 + 1 / 4 * Enermy.ChangeSa) * (Enermy.level * 2 / 5 + 2) / My.Sdefense * (1 + 1 / 4 * My.ChangeSd) / 50 + 2) * My.Type_check(self.type))
@@ -831,6 +872,7 @@ class Thunder(S_Skill):          # 번개
 class Peck(P_Skill):             # 쪼기
     def __init__(self):
         P_Skill.__init__(self, 35, 100, 35, 'Flight')
+        self.name = 'Peck'
 
     def Draw(self,check,My):
         for i in range(2,0,-1):
@@ -850,6 +892,7 @@ class Hypnosis:         # 최면술
         self.Daacur = 60
         self.Maxpp = 20
         self.type = 'Esper'
+        self.name = 'Hypnosis'
 
     def Use(self, My, Enermy):
         if (My.ailment == None):
@@ -858,6 +901,7 @@ class Hypnosis:         # 최면술
 class Take_Down(P_Skill):        # 돌진
     def __init__(self):
         P_Skill.__init__(self, 90, 85, 20, 'Normal')
+        self.name = 'Take Down'
 
     def Draw(self,check,My):
         for i in range(2,0,-1):
@@ -875,21 +919,26 @@ class Take_Down(P_Skill):        # 돌진
 class Low_Kick(P_Skill):         # 안다리걸기
     def __init__(self):
         P_Skill.__init__(self, 40, 100, 30, 'Fight')
+        self.name = 'Low Kick'
 
 
 class Karate_Chop(P_Skill):      # 태권당수
     def __init__(self):
         P_Skill.__init__(self, 70, 100, 25, 'Fight')
+        self.name = 'Karate Chop'
+
 
 
 class Cross_Chop(P_Skill):       # 크로스춉
     def __init__(self):
         P_Skill.__init__(self, 100, 80, 5, 'Fight')
+        self.name = 'Cross Chop'
 
 
 class Submission(P_Skill):       # 지옥의 바퀴
     def __init__(self):
         P_Skill.__init__(self, 80, 80, 25, 'Fight')
+        self.name = 'Submission'
 
 
 def init_skill():
