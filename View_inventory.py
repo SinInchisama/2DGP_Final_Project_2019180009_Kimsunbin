8aec2c99c9960a60ef8784b257846e798d0bb666
SinInchisama <sun98390123@gmail.com>
2022-11-15 23:12:51 +0900

    인벤토리를 보여주는 스테이트

--- View_inventory.py
+++ View_inventory.py
@@ -4,6 +4,7 @@ import play_state
 
 Cursory = None
 Cursoyx = None
+Minusx,Minusy = 0,0
 
 def enter():
     global Cursoyx,Cursory
@@ -11,11 +12,24 @@ def enter():
     pass
 
 def handle_events():
+    global Cursory,Cursorx
     events = get_events()
     for event in events:
         if event.type == SDL_KEYDOWN:
             if event.key ==SDLK_ESCAPE:
                 game_framework.pop_state()
+            elif event.key == SDLK_LEFT:
+                Cursorx -=1
+                if(Cursorx == -1):
+                    Cursorx = 3
+            elif event.key == SDLK_RIGHT:
+                Cursorx = (Cursorx + 1) %4
+            elif event.key == SDLK_LEFT:
+                pass
+            elif event.key == SDLK_RIGHT:
+                pass
+            elif event.key == SDLK_a:
+                print(play_state.hero.inventory.x,play_state.hero.inventory.y)
     pass
 
 def update():
