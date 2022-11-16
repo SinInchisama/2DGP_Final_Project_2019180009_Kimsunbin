78516eebd3e31ece18b19b689ee223d66652c433
SinInchisama <sun98390123@gmail.com>
2022-11-17 00:10:21 +0900

    Shop을 보여주는 state

---
+++ View_Shop.py
@@ -0,0 +1,30 @@
+import play_state
+import game_framework
+from pico2d import *
+
+def enter():
+    pass
+
+
+def draw():
+    clear_canvas()
+
+
+    delay(0.03)
+    update_canvas()
+    pass
+
+def handle_events():
+    pass
+
+def update():
+    pass
+
+def resume():
+    pass
+
+def pause():
+    pass
+
+def exit():
+    pass
