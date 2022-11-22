432b6f88653858dfc89cc03096215cf1d3279aec
SinInchisama <sun98390123@gmail.com>
2022-11-22 23:03:49 +0900

    Npc와 대화를 위한 스테이트

--- Dialog_state.py
+++ Dialog_state.py
@@ -1,17 +1,37 @@
 import play_state
 import game_framework
 from pico2d import *
+from Map import Maping
+import Font
+
+Diag_log = None
 
 def enter():
+    global Diag_log
+    Diag_log = Maping[play_state.round].Npc[play_state.hero.Meet_Npc].return_Diag()
     pass
 
 def handle_events():
+    global Diag_log
     events = get_events()
+    for event in events:
+        if event.type == SDL_KEYDOWN:
+            if event.key == SDLK_a:
+                Diag_log = Maping[play_state.round].Npc[play_state.hero.Meet_Npc].return_Diag()
+
 
 def update():
     pass
 
 def draw():
+    clear_canvas()
+
+    play_state.draw_world()
+    play_state.Board.clip_draw(0, 1, 83, 76, 320, 75, 644, 140)
+
+    Font.Draw_Al(*Diag_log,60,75,16,16)
+    update_canvas()
+
     pass
 
 def exit():
